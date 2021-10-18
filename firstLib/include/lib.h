e423522afe0e294f2fd4fef63949a767d0d1c60a
Jakub Szpilowski <jakub.szpilowski@student.pk.edu.pl>
2021-10-18 12:45:29 +0200

    First library and Cmake update

---
+++ firstLib/include/lib.h
@@ -0,0 +1,13 @@
+//
+// Created by Lenovo on 18.10.2021.
+//
+
+#ifndef FIRSTLIB_LIB_H
+#define FIRSTLIB_LIB_H
+
+#include <iostream>
+using namespace std;
+
+void isItWork();
+
+#endif //FIRSTLIB_LIB_H
---
+++ firstLib/src/lib.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Lenovo on 18.10.2021.
+//
+
+#include "lib.h"
+
+void isItWork(){
+    cout << "Yes it work!" << endl;
+}
