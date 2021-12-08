e2b79550d8a3da74055a26f115fb32b20c0cb4ed
Jakub Szpilowski <jakub.szpilowski@student.pk.edu.pl>
2021-12-08 13:39:33 +0100

    Iterator example (uppercase letters)

---
+++ iterators/exampleIterator/main.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Lenovo on 08.12.2021.
+//
+#include <iostream>
+#include <cctype>
+#include <list>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    list<char> word;
+    list<char>::iterator x;
+
+    /*
+    char* w = new char[20];
+    cout << "Enter a word: ";
+    cin >> w;
+*/
+    char a;
+    cout << "Keep writing characters (! will stop the loop)\n";
+    do{
+        cin >> a;
+        word.push_back(a);
+    }while(a != 33);
+
+    cout << "Before: ";
+    for(x = word.begin(); x != word.end(); x++)
+        cout << *x;
+    cout << endl;
+
+    for(x = word.begin(); x != word.end(); x++)
+        *x = toupper(*x);
+
+    cout << "After: ";
+    for(x = word.begin(); x != word.end(); x++)
+        cout << *x;
+    cout << endl;
+
+   // delete[] w;
+    return 0;
+}
