ed89f8587289726438209de423843104a588d6bf
Jakub Szpilowski <jakub.szpilowski@student.pk.edu.pl>
2022-01-31 16:38:48 +0100

    Release example

---
+++ matrixExample/include/TimeMeasure.h
@@ -0,0 +1,46 @@
+//
+// Created by Lenovo on 17.01.2022.
+//
+
+#ifndef JIPP2_TIMEMEASURE_H
+#define JIPP2_TIMEMEASURE_H
+#include <chrono>
+
+using namespace std;
+
+class TimeMeasure{
+private:
+    chrono::time_point<chrono::high_resolution_clock> start;
+    chrono::time_point<chrono::high_resolution_clock> end;
+
+public:
+    /**
+     * Register start timer time
+     */
+    void timerStart();
+
+    /**
+     * Register end timer time
+     */
+    void timerStop();
+
+    /**
+     * Calculate timer time and return it in seconds
+     * @return timer work time in seconds
+     */
+    long long int getTimerTimeInSeconds();
+
+    /**
+     * Calculate timer time and return it in milliseconds
+     * @return timer work time in milliseconds
+     */
+    long long int getTimerTimeInMilliseconds();
+
+    /**
+     * Calculate timer time and return it in nanoseconds
+     * @return timer work time in nanoseconds
+     */
+    long long int getTimerTimeInNanoseconds();
+};
+
+#endif //JIPP2_TIMEMEASURE_H
